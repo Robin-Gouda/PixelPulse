e9f212024e08107860ac68d45f67db69b55390d0
Robin Gouda <robin2contact@gmail.com>
2024-02-06 01:48:53 +0530

    share button

--- client/src/Components/PostSide/PostShare/PostShare.jsx
+++ client/src/Components/PostSide/PostShare/PostShare.jsx
@@ -9,10 +9,13 @@ import {
   UilPlayCircle,
   UilTimes,
 } from "@iconscout/react-unicons";
+import { useSelector } from "react-redux";
 
 const PostShare = () => {
   const [image, setImage] = useState(null);
   const imageRef = useRef();
+  const desc = useRef();
+  const { user } = useSelector((state) => state.authReducer.authData);
   const onImageChange = (e) => {
     if (e.target.files && e.target.files[0]) {
       let img = e.target.files[0];
@@ -20,11 +23,29 @@ const PostShare = () => {
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
+
+    if (image) {
+      const data = new FormData();
+      const filename = Date.now() + image.name;
+      data.append("name", filename);
+      data.append("file", image);
+      newPost.image = filename;
+      console.log(newPost);
+    }
+  };
+
   return (
     <div className="PostShare">
       <img src={ProfileImage} alt="profile_Image" />
       <div>
-        <input type="text" placeholder="What's happening" />
+        <input ref={desc} required type="text" placeholder="What's happening" />
         <div className="postOptions" style={{ color: "var(--photo)" }}>
           <div
             className="option"
@@ -46,7 +67,11 @@ const PostShare = () => {
             <UilSchedule />
             Schedule
           </div>
-          <button className="button ps-button">Share</button>
+
+          <button className="button ps-button" onClick={handleSubmit}>
+            Share
+          </button>
+
           <div style={{ display: "none" }}>
             <input
               type="file"
