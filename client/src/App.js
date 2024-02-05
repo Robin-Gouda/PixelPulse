2f71c62b86143b6d4e555afa6100edeefb8ef7f8
Robin Gouda <robin2contact@gmail.com>
2024-02-05 23:15:23 +0530

    frontend routing in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
 import Auth from "./Pages/Auth/Auth";
 import Profile from "./Pages/Profiles/Profile";
-// import Home from "./pages/home/home";
 import Home from "./Pages/home/home.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const user = useSelector((state) => state.authReducer.authData);
   return (
     <div className="App">
       <div className="blur blurone"></div>
       <div className="blur blurtwo"></div>
-      {/* <Home /> */}
-      {/* <Profile /> */}
-      <Auth />
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+        />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth />}
+        />
+      </Routes>
     </div>
   );
 }
