f35b965c1c3309dd3aa98a30aa58f9f75b85c410
Robin Gouda <robin2contact@gmail.com>
2024-02-07 21:09:00 +0530

    logical unit modification

--- client/src/Components/ProfileSide/FollowerCard/FollowerCard.jsx
+++ client/src/Components/ProfileSide/FollowerCard/FollowerCard.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./FollowerCard.css";
 import { Followers } from "../../../Data/FollowersData";
 import Users from "../../User/Users";
 
 const FollowerCard = () => {
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    const fetchPersons = async () => {};
+  }, []);
   return (
     <div className="FollowersCard">
       <h3>People you Would like to know</h3>
