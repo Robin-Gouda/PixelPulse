b30f0c36bf3ef4f99e6c75aaa7a2cd2a5eb5cf43
Robin Gouda <robin2contact@gmail.com>
2024-02-09 13:48:23 +0530

    follow and unFollow

--- client/src/Components/User/Users.jsx
+++ client/src/Components/User/Users.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { followUser, unFollowUser } from "../../Action/UserAction";
 
 const Users = ({ person }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
   const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  const handleFollow = () => {
+    dispatch(unFollowUser(person._id, user));
+  };
   return (
     <div className="follower">
       <div>
@@ -19,7 +27,9 @@ const Users = ({ person }) => {
           <span>{person.username}</span>
         </div>
       </div>
-      <button className="button fc-button">Follow</button>
+      <button className="button fc-button" onClick={handleFollow}>
+        Follow
+      </button>
     </div>
   );
 };
